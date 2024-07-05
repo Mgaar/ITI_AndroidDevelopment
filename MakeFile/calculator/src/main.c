2c810142ec448cba2c1581a571d969b124dc7d92
Mgaar <152877037+Mgaar@users.noreply.github.com>
2024-07-05 19:00:05 +0300

    main.c

---
+++ MakeFile/calculator/src/main.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include "calculator.h"
+
+void add (int num1,int num2)
+{
+printf("%d + %d =%d",num1,num2,num1+num2);
+
+}
+
+void sub (int num1,int num2)
+{
+printf("%d - %d =%d",num1,num2,num1-num2);
+
+}
+
+void mul (int num1,int num2)
+{
+printf("%d * %d =%d",num1,num2,num1*num2);
+
+}
+
+void dev (int num1,int num2)
+{
+printf("%d / %d =%d",num1,num2,num1/num2);
+
+}
