83261b38ff648c1dfb36aaeabe835c3ca5067120
Mgaar <152877037+Mgaar@users.noreply.github.com>
2024-07-05 21:29:33 +0300

    Update Q4.cpp

--- CPP/TASKS/task1/Q4.cpp
+++ CPP/TASKS/task1/Q4.cpp
@@ -14,6 +14,17 @@ int main()
 		people = people+((people*65)/1000);
 	}
 	cout << years << " years ";
-
-	
 }
+
+/*
+we can use this formula to clculate number of years without looping 
+P(t)=P(0)×(1+r)^t
+ 
+
+where:
+
+P(t) is the future population.
+𝑃(0) is the initial population (162,100).
+r is the annual growth rate (6.5% or 0.065).
+t is the number of years.
+*/
