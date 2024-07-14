a33596d98e7cfbf3055fb86373dfcc00a1d7e117
Mgaar <152877037+Mgaar@users.noreply.github.com>
2024-07-14 14:59:32 +0300

    Create q1.cpp

---
+++ CPP/TASKS/task3/q1.cpp
@@ -0,0 +1,30 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+class Vertex {
+    private :
+    int x;
+    int y;
+    public:
+    Vertex(void)
+    {
+
+    
+    // Generate random number between -100 and 100
+    x=(rand() % 201) - 100;
+    y=(rand() % 201) - 100;
+std::cout << "x="<<x<<" y="<<y;
+
+    }
+    
+};
+
+int main (){
+srand(static_cast<unsigned int>(time(0)));
+Vertex v1;
+Vertex v2;
+Vertex v3;
+
+
+    return 0;
+}
